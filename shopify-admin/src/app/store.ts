c28ea70644b411aac185be089116c846439a0cb1
michaworku <mikias27worku@gmail.com>
2022-03-25 13:47:10 +0300

    auth store

---
+++ shopify-admin/src/app/store.ts
@@ -0,0 +1,40 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+import authReducer from '../features/auth/authSlice'
+
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+  }
+// const rootReducer = combineReducers({auth: authReducer})
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
+export const store = configureStore({
+    reducer:{
+      auth:persistedReducer},
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export let persistor = persistStore(store);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
--- shopify-admin/src/main.tsx
+++ shopify-admin/src/main.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import ReactDOM from "react-dom";
+import App from "./App";
+import { persistor, store,  } from './app/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 import { DarkModeContextProvider } from './context/darkModeContext'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <DarkModeContextProvider>
-      <App />
-    </DarkModeContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+    <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <DarkModeContextProvider>
+            <App />
+          </DarkModeContextProvider>
+        </PersistGate>
+    </Provider>,
+    document.getElementById("root"));
+
