b3a48a2a0ea62b742db6433ac289c83dcd6815b2
michaworku <mikias27worku@gmail.com>
2022-03-27 22:46:29 +0300

    creating products and users with firebase storage

---
+++ shopify-admin/src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD-GWe9Ui5MJESQDPVeHgWMIMiT_4cQpb0",
+  authDomain: "shopify-1216192127.firebaseapp.com",
+  projectId: "shopify-1216192127",
+  storageBucket: "shopify-1216192127.appspot.com",
+  messagingSenderId: "374185578227",
+  appId: "1:374185578227:web:f06bcdb441b0d1b153b186"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
