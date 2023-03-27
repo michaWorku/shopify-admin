cc085106d3dbe29cdc939190d9022ec6ab6725c8
mikias.wk <mikias.wk@2f-capital.com>
2023-03-27 14:54:04 +0300

    append get entities service - Retrieves entities based on the user permissions.

--- app/services/Entities/entity.server.ts
+++ app/services/Entities/entity.server.ts
@@ -1,5 +1,6 @@
 import { Response, errorHandler } from "~/utils/handler.server";
 import { db } from "../db.server";
+import canUser from "~/utils/casl/ability";
 
 /**
  * Get all entities for the specified model.
@@ -25,7 +26,6 @@ export const getAllEntities = async (model: string): Promise<object> => {
     }
 }
 
-
 /**
  * Retrieves users of a given model based on user ID.
  * @async function getUserEnities
@@ -34,7 +34,7 @@ export const getAllEntities = async (model: string): Promise<object> => {
  * @returns {Promise<object>} - The response data containing the found users.
  * @throws {Error} - If an invalid model is provided.
  */
- export const getUserEntities = async (model: string, userId: string): Promise<object> => {
+export const getUserEntities = async (model: string, userId: string): Promise<object> => {
     try {
         if (!model) {
             throw new Error("Invalid model provided");
@@ -59,3 +59,39 @@ export const getAllEntities = async (model: string): Promise<object> => {
         return errorHandler(e);
     }
 };
+
+/**
+ * Retrieves entities based on user permissions.
+ * @param {string} userId - The ID of the user.
+ * @param {string} entity - The name of the entity to retrieve.
+ * @returns {Promise<object>} - An object containing the retrieved entities.
+ * @throws {Error} - If an invalid model is provided.
+ */
+export const getEntities = async (userId: string, entity: string): Promise<object> => {
+    try {
+        // Check if a valid entity name is provided.
+        if (!entity) {
+            throw new Error("Invalid model provided");
+        }
+
+        // Check if the user has permission to view all entities.
+        const iCanViewAll = await canUser(userId, 'read', 'Permission', {});
+
+        let entities: any;
+
+        if (iCanViewAll?.status === 200) {
+            // Retrieve all entities if the user has permission to view all.
+            entities = await getAllEntities(entity);
+        } else if (iCanViewAll?.status === 403) {
+            // Retrieve only the entities that the user has permission to view.
+            entities = await getUserEntities(userId, entity);
+        } else {
+            // Return the permission error response.
+            return iCanViewAll;
+        }
+
+        return entities;
+    } catch (e) {
+        return errorHandler(e);
+    }
+}
