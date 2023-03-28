10b8cab58a33b5dd0074996fc9a4e1834a135627
mikias.wk <mikias.wk@2f-capital.com>
2023-03-28 14:29:28 +0300

    Retrieves all clients based on the specified request parameters.

--- app/services/Client/client.server.ts
+++ app/services/Client/client.server.ts
@@ -1,9 +1,12 @@
 import { Client, Permission } from "@prisma/client"
 import canUser from "~/utils/casl/ability"
-import { Response, ResponseType, errorHandler } from "~/utils/handler.server"
+import customErr, { Response, ResponseType, errorHandler } from "~/utils/handler.server"
 import { db } from "../db.server"
 import clientPermissions from "~/utils/permissionRepo/clientPermissions"
 import interpolate from "~/utils/casl/interpolate"
+import getParams from "~/utils/params/getParams.server"
+import { filterFunction } from "~/utils/params/filter.server"
+import { searchFunction } from "~/utils/params/search.server"
 
 /**
  * Creates a new client with the provided data.
@@ -64,3 +67,103 @@ export const createClient = async (clientData: Client, userId: string): Promise<
         return errorHandler(err)
     }
 }
+
+
+/**
+ * Retrieves all clients based on the specified request parameters.
+ * @async function getAllClients
+ * @param {Request} request - The HTTP request object containing the request parameters.
+ * @param {string} [userId] - The ID of the user, if any, that is associated with the clients.
+ * @returns {Promise<Response>} - The HTTP response containing the clients and their metadata.
+ * @throws {customErr} - An error indicating that no clients were found.
+ * @throws {Error} - An error indicating that an unexpected error occurred.
+ */
+export const getAllClients = async (request: Request, userId?: string): Promise<ResponseType> => {
+    try {
+        const {
+            sortType,
+            sortField,
+            skip,
+            take,
+            pageNo,
+            search,
+            filter,
+            exportType,
+        } = getParams(request);
+
+        const searchParams = searchFunction(search, 'Client', ['name', 'phone']); // Adjust the search columns as necessary.
+        const filterParams = filterFunction(filter, 'Client');
+
+        let _where: any = {
+            deletedAt: null,
+            ...(userId
+                ? {
+                    users: {
+                        some: {
+                            id: userId
+                        }
+                    },
+                }
+                : {}),
+            ...searchParams,
+            ...filterParams,
+        };
+
+        const clientsCount = await db.client.count({
+            where: _where,
+        });
+
+        if (clientsCount > 0) {
+            const clients = await db.client.findMany({
+                take,
+                skip,
+                orderBy: [
+                    {
+                        [sortField]: sortType,
+                    },
+                ],
+                where: _where,
+            });
+
+            let exportData;
+
+            if (exportType === 'page') {
+                exportData = clients;
+            } else if (exportType === 'filtered') {
+                exportData = await db.client.findMany({
+                    orderBy: [
+                        {
+                            ...(sortField !== 'name'
+                                ? { [sortField]: sortType }
+                                : {
+                                    name: sortType,
+                                }),
+                        },
+                    ],
+                    where: _where,
+                });
+            } else {
+                exportData = await db.client.findMany({});
+            }
+
+            return Response({
+                data: clients,
+                metaData: {
+                    page: pageNo,
+                    pageSize: take,
+                    total: clientsCount,
+                    sort: [sortField, sortType],
+                    searchVal: search,
+                    filter,
+                    exportType,
+                    exportData
+                },
+            });
+        }
+
+        throw new customErr('Custom_Error', 'No clients found', 404);
+    } catch (error) {
+        return errorHandler(error);
+    }
+};
+
