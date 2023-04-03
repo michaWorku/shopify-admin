7aac17fcc9ed58ebb818c708ac2d6f7e55f77f43
mikias.wk <mikias.wk@2f-capital.com>
2023-04-03 16:36:43 +0300

    export default for components

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -1,1 +1,4 @@
 export {default as Layout} from './Layout'
+export {default as  ClientForm} from './ClientForm'
+export {default as DeleteAlert} from './DeleteAlert'
+export {default as ControlledTextField} from './ControlledTextField'
