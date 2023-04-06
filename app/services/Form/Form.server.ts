33da4d1a0bcd9438d2c645f1bc5c671df6a06824
mikias.wk <mikias.wk@2f-capital.com>
2023-04-06 13:45:46 +0300

    refactor form server

--- app/services/Form/Form.server.ts
+++ app/services/Form/Form.server.ts
@@ -11,10 +11,20 @@ import { canPerformAction } from "~/utils/casl/canPerformAction"
 interface DynamicForm {
     name: string;
     description: string;
-    createdBy: number;
-    clientId: number;
-    fields: DynamicFormField[];
+    fields: any;
 }
+
+type DynamicFormFieldUpdateManyData = {
+    name?: string;
+    label?: string;
+    type?: string;
+    defaultValue?: string;
+    required?: boolean;
+    placeholder?: string;
+    description?: string;
+    order?: number;
+}
+
 /**
  * Creates a dynamic form with a set of form fields
  * @param {DynamicForm} formData - The data of the dynamic form to create
@@ -29,16 +39,26 @@ export const createForm = async (formData: DynamicForm, userId: string, clientId
             return canCreate
         }
 
+        if (!formData.name || !formData.fields) {
+            throw new customErr("Custom_Error", "Invalid form data!", 400);
+        }
+
         // Create form with associated fields
         const data = await db.dynamicForm.create({
             data: {
                 name: formData.name,
                 description: formData.description,
                 createdBy: userId,
-                clientId,
-                fields: {
-                    create: (formData?.fields as any)?.map((field: DynamicFormField) => ({ data: field })),
+                client: {
+                    connect: {
+                        id: clientId
+                    }
                 },
+                fields: {
+                    createMany: {
+                        data: formData?.fields as any
+                    }
+                }
             },
             include: {
                 fields: true
@@ -55,7 +75,6 @@ export const createForm = async (formData: DynamicForm, userId: string, clientId
         return errorHandler(err)
     }
 }
-
 
 /**
  * Retrieves all dynamic forms for the specified client based on the specified request parameters.
@@ -91,7 +110,7 @@ export const getAllClientDynamicForms = async (request: Request, clientId?: stri
             skip,
             orderBy: [{ [sortField]: sortType }],
             where: dynamicFormsWhere,
-            include:{
+            include: {
                 fields: true
             }
         });
@@ -109,7 +128,7 @@ export const getAllClientDynamicForms = async (request: Request, clientId?: stri
             exportData = await db.dynamicForm.findMany({});
         }
 
-        return json(Response({
+        return Response({
             data: dynamicForms,
             metaData: {
                 page: pageNo,
@@ -121,7 +140,7 @@ export const getAllClientDynamicForms = async (request: Request, clientId?: stri
                 exportType,
                 exportData,
             },
-        }), { status: 200 })
+        })
     } catch (error) {
         console.log('Error occurred loading dynamic forms.');
         console.dir(error, { depth: null });
@@ -133,15 +152,15 @@ export const getAllClientDynamicForms = async (request: Request, clientId?: stri
  * Updates a dynamic form field by ID
  * @async
  * @function updateDynamicFormById
- * @param {string} dynamicFormId - ID of the dynamic form to update
+ * @param {string} formId - ID of the dynamic form to update
  * @param {object} data - Updated dynamic form field data
  * @param {string} userId - The ID of the user updating the dynamic form
  * @param {string} clientId - The ID of the client associated with the forms.
  * @returns {Promise<object>} Updated dynamic form  object
  * @throws {customErr} If no dynamic form found with given ID
  */
-export const updateDynamicFormById = async (dynamicFormId: string, data: any, userId: string, clientId: string): Promise<any> => {
-    if (!dynamicFormId) {
+export const updateDynamicFormById = async (formId: string, data: DynamicForm, userId: string, clientId: string): Promise<any> => {
+    if (!formId) {
         throw new customErr('Custom_Error', 'Dynamic form field ID is required', 404);
     }
 
@@ -153,9 +172,58 @@ export const updateDynamicFormById = async (dynamicFormId: string, data: any, us
     }
 
     try {
-        const updatedDynamicFormField = await db.dynamicFormField.update({
-            where: { id: dynamicFormId },
-            data,
+        const updatedDynamicFormField = await db.dynamicForm.upsert({
+            where: { id: formId },
+            update: {
+                name: data.name,
+                description: data.description,
+                createdBy: userId,
+                fields: {
+                    updateMany:
+                        data?.fields?.map((field: any) => ({
+                            where: { id: field.id },
+                            data: {
+                                name: field.name,
+                                label: field.label,
+                                type: field.type,
+                                defaultValue: field.defaultValue,
+                                required: field.required,
+                                placeholder: field.placeholder,
+                                description: field.description,
+                                order: field.order,
+                            },
+                        })),
+
+                },
+            },
+            create: {
+                name: data?.name,
+                description: data?.description,
+                createdBy: userId,
+                client: {
+                    connect: {
+                        id: clientId
+                    }
+                },
+                fields: {
+                    createMany: {
+                        data: data?.fields?.map((field: any) => ({
+                            name: field?.name,
+                            label: field?.label,
+                            type: field?.type,
+                            defaultValue: field?.defaultValue,
+                            required: field?.required,
+                            placeholder: field?.placeholder,
+                            description: field?.description,
+                            order: field?.order,
+                        })),
+
+                    },
+                }
+            },
+            include: {
+                fields: true,
+            },
         });
 
         return json(Response({
@@ -167,19 +235,20 @@ export const updateDynamicFormById = async (dynamicFormId: string, data: any, us
     }
 };
 
+
 /**
  * Deletes a dynamic form with the given ID.
  * @async
  * @function deleteDynamicForm
- * @param {string} dynamicFormId - The ID of the dynamic form to delete.
+ * @param {string} formId - The ID of the dynamic form to delete.
  * @param {string} userId - The ID of the user deleting the dynamic form.
  * @param {string} clientId - The ID of the client associated with the forms. 
 * @returns {Promise<object>} The deleted dynamic form object.
  * @throws {Error} If no dynamic form is found with the given ID.
  */
-export const deleteDynamicForm = async (dynamicFormId: string, userId: string, clientId: string): Promise<any> => {
+export const deleteDynamicForm = async (formId: string, userId: string, clientId: string): Promise<any> => {
     try {
-        if (!dynamicFormId) {
+        if (!formId) {
             throw new customErr('Custom_Error', 'Dynamic Form ID is required', 404);
         }
 
@@ -193,7 +262,7 @@ export const deleteDynamicForm = async (dynamicFormId: string, userId: string, c
 
         const dynamicForm = await db.dynamicForm.update({
             where: {
-                id: String(dynamicFormId),
+                id: String(formId),
             },
             data: {
                 deletedAt: new Date(),
@@ -225,48 +294,47 @@ export const deleteDynamicForm = async (dynamicFormId: string, userId: string, c
  * @returns {Promise<Object>} An object containing the retrieved dynamic forms with associated permissions and capabilities.
  * @throws {customErr} Throws a custom error if the user does not have permission to access the dynamic forms, or if an error occurs while retrieving them.
  */
- export const getDynamicForms = async (request: Request, userId: string, clientId: string): Promise<Object> => {
+export const getDynamicForms = async (request: Request, userId: string, clientId: string): Promise<Object> => {
     try {
-      const canViewDynamicForms = await canUser(userId, 'read', 'DynamicForm', {});
-      if (canViewDynamicForms?.status !== 200) {
-        const canViewDynamicFormsPartial = await canUser(userId, 'read', 'DynamicForm', {}, AbilityType.PARTIAL);
-        if (!canViewDynamicFormsPartial?.ok) {
-          return canViewDynamicFormsPartial;
+        const canViewDynamicForms = await canUser(userId, 'read', 'DynamicForm', {});
+        if (canViewDynamicForms?.status !== 200) {
+            const canViewDynamicFormsPartial = await canUser(userId, 'read', 'DynamicForm', {}, AbilityType.PARTIAL);
+            if (!canViewDynamicFormsPartial?.ok) {
+                return canViewDynamicFormsPartial;
+            }
         }
-      }
-      const dynamicForms = await getAllClientDynamicForms(request, clientId);
-      if (dynamicForms?.data) {
-        await setDynamicFormPermissions(userId, dynamicForms.data, clientId);
-      }
-      return dynamicForms;
+        const dynamicForms = await getAllClientDynamicForms(request, clientId);
+        if (dynamicForms?.data) {
+            await setDynamicFormPermissions(userId, dynamicForms.data, clientId);
+        }
+        return dynamicForms;
     } catch (err) {
-      return errorHandler(err);
+        return errorHandler(err);
     }
-  };
-  
-  /**
-   * Sets Dynamic form permissions for a given client.
-   *
-   * @async
-   * @function setDynamicFormPermissions
-   * @param {string} userId - The ID of the user to set permissions for.
-   * @param {Object[]} dynamicForms - An array of dynamic forms to set permissions for.
-   * @param {string} clientId - The ID of the client to set permissions for.
-   * @returns {Promise<void>} A promise that resolves when the permissions have been set.
-   * @throws {customErr} Throws a custom error if the client or user cannot be found.
-   */
-  const setDynamicFormPermissions = async (userId: string, dynamicForms: any[], clientId: string): Promise<void> => {
+};
+
+/**
+ * Sets Dynamic form permissions for a given client.
+ *
+ * @async
+ * @function setDynamicFormPermissions
+ * @param {string} userId - The ID of the user to set permissions for.
+ * @param {Object[]} dynamicForms - An array of dynamic forms to set permissions for.
+ * @param {string} clientId - The ID of the client to set permissions for.
+ * @returns {Promise<void>} A promise that resolves when the permissions have been set.
+ * @throws {customErr} Throws a custom error if the client or user cannot be found.
+ */
+const setDynamicFormPermissions = async (userId: string, dynamicForms: any[], clientId: string): Promise<void> => {
     const promises = dynamicForms.map(async (dynamicFormData: any, index: number) => {
-      const canEdit = await canPerformAction(userId, 'update', 'DynamicForm', { clientId });
-      const canViewFormField = await canPerformAction(userId, 'read', 'DynamicFormField', { clientId });
-      const canDelete = await canPerformAction(userId, 'delete', 'DynamicForm', { clientId });
-      dynamicForms[index] = {
-        ...dynamicFormData,
-        canEdit,
-        canDelete,
-        canViewFormField
-      };
+        const canEdit = await canPerformAction(userId, 'update', 'DynamicForm', { clientId });
+        const canViewFormFields = await canPerformAction(userId, 'read', 'DynamicFormField', { clientId });
+        const canDelete = await canPerformAction(userId, 'delete', 'DynamicForm', { clientId });
+        dynamicForms[index] = {
+            ...dynamicFormData,
+            canEdit,
+            canDelete,
+            canViewFormFields
+        };
     });
     await Promise.all(promises);
-  };
-  
+};
