0f5b475f452e1b1452fc76a22a710fe6d7695300
mikias.wk <mikias.wk@2f-capital.com>
2023-04-13 12:40:04 +0300

    add string format utils

--- app/utils/format.ts
+++ app/utils/format.ts
@@ -8,3 +8,14 @@ export function formatFileSize(bytes: number) {
 }
 
 export const format = "ddd, DD MMM YYYY HH:mm:ss";
+
+/**
+ * Separates a camel case string with capital letters and makes the first letter capitalized.
+ * @param {string} camelCaseString - The camel case string to be separated and capitalized.
+ * @returns {string} The separated and capitalized string.
+ */
+export const separateAndCapitalize = (camelCaseString: string) => {
+    const separatedString = camelCaseString.replace(/([A-Z])/g, " $1");
+    const capitalizedString = separatedString.charAt(0).toUpperCase() + separatedString.slice(1);
+    return capitalizedString;
+}
