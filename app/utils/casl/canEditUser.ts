c10d819f755701c409460a4e5080c829c2da347a
samri <gonesam77@gmail.com>
2023-04-14 22:11:29 +0300

    fix auth issue => check for manage all permission on user action

--- app/utils/casl/canEditUser.ts
+++ app/utils/casl/canEditUser.ts
@@ -7,30 +7,35 @@ export async function canEditUser(
 ) {
     let edit: any
     let flag = false
-    if (!client) {
-        edit = await canUser(editorId, 'update', 'SystemUser', {})
-        if (edit.status === 200) {
-            edit = await canUser(editedId, 'update', 'SystemUser', {})
-            if (!edit.ok) {
-                flag = true
-            }
-        }
+    edit = await canUser(editorId, 'manage', 'all', {})
+    if (edit?.ok) {
+        flag = true
     } else {
-        /*eslint-disable*/
-        edit = await canUser(editorId, 'update', 'SystemUser', {
-            clientId: client,
-        })
-        console.log({
-            editedId,
-            editorId,
-            edit: JSON.stringify(await edit.json()),
-        })
-        if (edit.status === 200) {
-            edit = await canUser(editedId, 'update', 'SystemUser', {
+        if (!client) {
+            edit = await canUser(editorId, 'update', 'SystemUser', {})
+            if (edit.status === 200) {
+                edit = await canUser(editedId, 'update', 'SystemUser', {})
+                if (!edit.ok) {
+                    flag = true
+                }
+            }
+        } else {
+            /*eslint-disable*/
+            edit = await canUser(editorId, 'update', 'SystemUser', {
                 clientId: client,
             })
-            if (!edit.ok) {
-                flag = true
+            console.log({
+                editedId,
+                editorId,
+                edit: JSON.stringify(await edit.json()),
+            })
+            if (edit.status === 200) {
+                edit = await canUser(editedId, 'update', 'SystemUser', {
+                    clientId: client,
+                })
+                if (!edit.ok) {
+                    flag = true
+                }
             }
         }
     }
