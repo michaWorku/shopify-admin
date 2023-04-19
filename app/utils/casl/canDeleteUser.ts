5bf21f7f7d491a5d8357a0866b679bebd319d095
samri <gonesam77@gmail.com>
2023-04-19 16:24:55 +0300

    update edit status => change edit.ok to edit.status

--- app/utils/casl/canDeleteUser.ts
+++ app/utils/casl/canDeleteUser.ts
@@ -1,39 +1,39 @@
-import canUser, { AbilityType } from './ability'
+import canUser, { AbilityType } from "./ability";
 
 export async function canDeleteUser(
-    editorId: string,
-    editedId: string,
-    client?: {}
+  editorId: string,
+  editedId: string,
+  client?: {}
 ) {
-    let edit: any
-    let flag = false
-    edit = await canUser(editorId, 'manage', 'all', {})
-    if (edit?.ok) {
-        flag = true
-    } else {
-        if (!client) {
-            edit = await canUser(editorId, 'delete', 'SystemUser', {})
-            if (edit?.status === 200) {
-                edit = await canUser(editedId, 'delete', 'SystemUser', {})
-                if (!edit.ok) {
-                    flag = true
-                }
-            }
-        } else {
-            /*eslint-disable*/
-            edit = await canUser(editorId, 'delete', 'SystemUser', {
-                clientId: client,
-            })
-            if (edit?.status === 200) {
-                edit = await canUser(editedId, 'delete', 'SystemUser', {
-                    clientId: client,
-                })
-                if (!edit.ok) {
-                    flag = true
-                }
-            }
+  let edit: any;
+  let flag = false;
+  edit = await canUser(editorId, "manage", "all", {});
+  if (edit?.ok) {
+    flag = true;
+  } else {
+    if (!client) {
+      edit = await canUser(editorId, "delete", "SystemUser", {});
+      if (edit?.status === 200) {
+        edit = await canUser(editedId, "delete", "SystemUser", {});
+        if (edit?.status != 200) {
+          flag = true;
         }
+      }
+    } else {
+      /*eslint-disable*/
+      edit = await canUser(editorId, "delete", "SystemUser", {
+        clientId: client,
+      });
+      if (edit?.status === 200) {
+        edit = await canUser(editedId, "delete", "SystemUser", {
+          clientId: client,
+        });
+        if (edit?.status != 200) {
+          flag = true;
+        }
+      }
     }
+  }
 
-    return flag
+  return flag;
 }
--- app/utils/casl/canEditUser.ts
+++ app/utils/casl/canEditUser.ts
@@ -1,44 +1,39 @@
-import canUser from './ability'
+import canUser from "./ability";
 
 export async function canEditUser(
-    editorId: string,
-    editedId: string,
-    client?: any
+  editorId: string,
+  editedId: string,
+  client?: any
 ) {
-    let edit: any
-    let flag = false
-    edit = await canUser(editorId, 'manage', 'all', {})
-    if (edit?.ok) {
-        flag = true
-    } else {
-        if (!client) {
-            edit = await canUser(editorId, 'update', 'SystemUser', {})
-            if (edit.status === 200) {
-                edit = await canUser(editedId, 'update', 'SystemUser', {})
-                if (!edit.ok) {
-                    flag = true
-                }
-            }
-        } else {
-            /*eslint-disable*/
-            edit = await canUser(editorId, 'update', 'SystemUser', {
-                clientId: client,
-            })
-            console.log({
-                editedId,
-                editorId,
-                edit: JSON.stringify(await edit.json()),
-            })
-            if (edit.status === 200) {
-                edit = await canUser(editedId, 'update', 'SystemUser', {
-                    clientId: client,
-                })
-                if (!edit.ok) {
-                    flag = true
-                }
-            }
+  let edit: any;
+  let flag = false;
+  edit = await canUser(editorId, "manage", "all", {});
+  if (edit?.ok) {
+    flag = true;
+  } else {
+    if (!client) {
+      edit = await canUser(editorId, "update", "SystemUser", {});
+      if (edit.status === 200) {
+        edit = await canUser(editedId, "update", "SystemUser", {});
+        if (edit?.status != 200) {
+          flag = true;
         }
+      }
+    } else {
+      /*eslint-disable*/
+      edit = await canUser(editorId, "update", "SystemUser", {
+        clientId: client,
+      });
+      if (edit.status === 200) {
+        edit = await canUser(editedId, "update", "SystemUser", {
+          clientId: client,
+        });
+        if (edit?.status != 200) {
+          flag = true;
+        }
+      }
     }
+  }
 
-    return flag
+  return flag;
 }
