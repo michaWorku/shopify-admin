d9dc8e708e55f3cd1f9b03b119d00f9fce48a301
samri <gonesam77@gmail.com>
2023-04-19 14:32:11 +0300

    Use formHandler method => replace validate function  with formHandler

--- app/routes/clients/$clientId/systemusers/__systemusers/addNew.tsx
+++ app/routes/clients/$clientId/systemusers/__systemusers/addNew.tsx
@@ -1,87 +1,70 @@
-import { useActionData, useNavigate } from '@remix-run/react'
-import { useEffect } from 'react'
-import type { LoaderFunction, ActionFunction} from '@remix-run/server-runtime';
-import { json } from '@remix-run/server-runtime'
-import { authenticator } from '~/services/auth.server'
-import { createSystemUser } from '~/services/User/systemuser.server'
-import { Response, errorHandler } from '~/utils/handler.server'
-import AddUserForm from '~/src/components/Forms/AddUserForm'
-import { validate } from '~/utils/validators/validate'
-import { systemUserSchema } from '~/utils/schema/systemUserSchema'
-import { toast } from 'react-toastify'
-import { getUserById } from '~/services/User/users.server'
-import { getUserCreatedRole } from '~/services/Role/role.server'
+import { useActionData, useNavigate } from "@remix-run/react";
+import { useEffect } from "react";
+import type { LoaderFunction, ActionFunction } from "@remix-run/server-runtime";
+import { json } from "@remix-run/server-runtime";
+import { authenticator } from "~/services/auth.server";
+import { createSystemUser } from "~/services/User/systemuser.server";
+import { Response, errorHandler } from "~/utils/handler.server";
+import AddUserForm from "~/src/components/Forms/AddUserForm";
+import { validate } from "~/utils/validators/validate";
+import { systemUserSchema } from "~/utils/schema/systemUserSchema";
+import { toast } from "react-toastify";
+import { getUserById } from "~/services/User/users.server";
+import { getUserCreatedRole } from "~/services/Role/role.server";
+import { formHandler } from "~/utils/formHandler";
 
 export const loader: LoaderFunction = async ({ request, params }) => {
-    try {
-        const user = await authenticator.isAuthenticated(request, {
-            failureRedirect: '/login',
-        })
-        const userData = (await getUserById(user?.id)) as any
-        const { roles } = await getUserCreatedRole(user?.id)
-        userData.userRoles = roles
-        return json(
-            Response({
-                data: userData,
-            })
-        )
-    } catch (err) {
-        return errorHandler(err)
-    }
-}
+  try {
+    const user = await authenticator.isAuthenticated(request, {
+      failureRedirect: "/login",
+    });
+    const userData = (await getUserById(user?.id)) as any;
+    const { roles } = await getUserCreatedRole(user?.id);
+    userData.userRoles = roles;
+    return json(
+      Response({
+        data: userData,
+      })
+    );
+  } catch (err) {
+    return errorHandler(err);
+  }
+};
 
 export const action: ActionFunction = async ({ request, params }) => {
-    // Authenticate the user
-    try {
-        const user = await authenticator.isAuthenticated(request, {
-            failureRedirect: '/login',
-        })
-        const form = await request.formData()
-        const userData = Object.fromEntries(form) as any
-        if (userData?.roleId) {
-            userData.roleId = JSON.parse(userData.roleId)
-        }
-        const { success, data, ...fieldError } = await validate(
-            userData,
-            systemUserSchema
-        )
-        if (!success) {
-            return json(
-                Response({
-                    error: {
-                        fieldError: [fieldError],
-                        error: { message: 'Invalid user Input fields' },
-                    },
-                }),
-                { status: 422 }
-            )
-        } else {
-            const response = await createSystemUser(
-                user?.id,
-                data,
-                params?.clientId
-            )
-            if (response.status == 200) {
-            }
-            return response
-        }
-    } catch (err) {
-        return await errorHandler(err)
+  // Authenticate the user
+  try {
+    const user = await authenticator.isAuthenticated(request, {
+      failureRedirect: "/login",
+    });
+    const result = (await formHandler(request, systemUserSchema)) as any;
+    if (!result?.success) {
+      return result;
+    } else {
+      const response = await createSystemUser(
+        user?.id,
+        result?.data,
+        params?.clientId
+      );
+      return response;
     }
-}
+  } catch (err) {
+    return await errorHandler(err);
+  }
+};
 export default function Users() {
-    const actionData = useActionData<typeof action>()
-    const navigate = useNavigate()
+  const actionData = useActionData<typeof action>();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if (actionData?.data?.message) {
-            toast.success(actionData?.data?.message)
-            navigate(-1)
-        }
-        if (actionData?.error) {
-            toast.error(actionData?.error?.error?.message)
-        }
-    }, [actionData])
+  useEffect(() => {
+    if (actionData?.data?.message) {
+      toast.success(actionData?.data?.message);
+      navigate(-1);
+    }
+    if (actionData?.error) {
+      toast.error(actionData?.error?.error?.message);
+    }
+  }, [actionData]);
 
-    return <AddUserForm openModal={true} closeModal={() => navigate(-1)} />
+  return <AddUserForm openModal={true} closeModal={() => navigate(-1)} />;
 }
