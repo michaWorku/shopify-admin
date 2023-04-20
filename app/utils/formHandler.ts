b418f39be8ff4714cfd8daba9082f174cddc683b
samri <gonesam77@gmail.com>
2023-04-20 10:18:12 +0300

    formHandler => parse roleId to array before validation

--- app/utils/formHandler.ts
+++ app/utils/formHandler.ts
@@ -1,7 +1,7 @@
-import { json } from '@remix-run/node'
+import { json } from "@remix-run/node";
 import { validate } from "./validators/validate";
-import { Response } from './handler.server';
-import type { z } from 'zod';
+import { Response } from "./handler.server";
+import type { z } from "zod";
 
 /**
  * Parses a multipart form data and validates it against the specified schema.
@@ -12,42 +12,49 @@ import type { z } from 'zod';
  * @returns {Promise<object>} - A Promise that resolves to an object containing the parsed and validated form data.
  * @throws {Error} - Throws an error if the validation fails.
  */
-export const formHandler = async (request: Request, schema: z.ZodObject<any>) => {
-    const form = await request.formData();
-    let formData = Object.fromEntries(form) as any;
+export const formHandler = async (
+  request: Request,
+  schema: z.ZodObject<any>
+) => {
+  const form = await request.formData();
+  let formData = Object.fromEntries(form) as any;
 
-    formData = JSON.parse(formData.data)
-    console.dir(formData, { depth: null });
-    const hasPhone = formData.hasOwnProperty('phone');
-
-    if (hasPhone) {
-        const parsedPhone = formData.phone.startsWith("+") ? formData.phone.slice(1) : formData.phone;
-        const phone = `251${parsedPhone.startsWith("0")
-            ? parsedPhone.slice(1)
-            : parsedPhone.startsWith("251")
-                ? parsedPhone.slice(3)
-                : parsedPhone
-            }`;
-        formData = { ...formData, phone }
+  formData = JSON.parse(formData.data);
+  console.dir(formData, { depth: null });
+  const hasPhone = formData.hasOwnProperty("phone");
+  Object.entries(formData).map(async ([key, value]) => {
+    if (key === "roleId") {
+      formData[key] = JSON.parse(value);
     }
-    const { success, data, ...fieldError } = await validate(
-        formData,
-        schema
+  });
+  if (hasPhone) {
+    const parsedPhone = formData.phone.startsWith("+")
+      ? formData.phone.slice(1)
+      : formData.phone;
+    const phone = `251${
+      parsedPhone.startsWith("0")
+        ? parsedPhone.slice(1)
+        : parsedPhone.startsWith("251")
+        ? parsedPhone.slice(3)
+        : parsedPhone
+    }`;
+    formData = { ...formData, phone };
+  }
+  const { success, data, ...fieldError } = await validate(formData, schema);
+  console.dir(fieldError, { depth: null });
+  if (!success) {
+    return json(
+      Response({
+        error: {
+          fieldError,
+          error: { message: "Validation error" },
+        },
+      }),
+      { status: 422 }
     );
-    console.dir(fieldError, { depth: null });
-    if (!success) {
-        return json(
-            Response({
-                error: {
-                    fieldError,
-                    error: { message: "Validation error" },
-                },
-            }),
-            { status: 422 }
-        );
-    }
-    return { success, data }
-}
+  }
+  return { success, data };
+};
 
 /**
  * Parses a request's form data, optionally transforming a phone number to the Ethiopian format.
@@ -56,36 +63,38 @@ export const formHandler = async (request: Request, schema: z.ZodObject<any>) =>
  * @returns {Promise<{ data: any }>} A Promise that resolves to an object with a `data` property containing the parsed form data.
  */
 export const requestFormHandler = async (request: Request) => {
-    const form = await request.formData();
-    let formData = Object.fromEntries(form) as any;
+  const form = await request.formData();
+  let formData = Object.fromEntries(form) as any;
 
-    formData = JSON.parse(formData.data)
-    console.dir(formData, { depth: null });
-    const hasPhone = formData.hasOwnProperty('phone');
+  formData = JSON.parse(formData.data);
+  console.dir(formData, { depth: null });
+  const hasPhone = formData.hasOwnProperty("phone");
 
-    if (hasPhone) {
-        const parsedPhone = formData.phone.startsWith("+") ? formData.phone.slice(1) : formData.phone;
-        const phone = `251${parsedPhone.startsWith("0")
-            ? parsedPhone.slice(1)
-            : parsedPhone.startsWith("251")
-                ? parsedPhone.slice(3)
-                : parsedPhone
-            }`;
-        formData = { ...formData, phone }
-    }
+  if (hasPhone) {
+    const parsedPhone = formData.phone.startsWith("+")
+      ? formData.phone.slice(1)
+      : formData.phone;
+    const phone = `251${
+      parsedPhone.startsWith("0")
+        ? parsedPhone.slice(1)
+        : parsedPhone.startsWith("251")
+        ? parsedPhone.slice(3)
+        : parsedPhone
+    }`;
+    formData = { ...formData, phone };
+  }
 
-    if (!Object.keys(formData).length) {
-        return json(
-            Response({
-                error: {
-                    error: { message: "Invalid data" },
-                },
-            }),
-            { status: 422 }
-        );
-    }
-    return {
-        data: formData,
-    }
-}
-
+  if (!Object.keys(formData).length) {
+    return json(
+      Response({
+        error: {
+          error: { message: "Invalid data" },
+        },
+      }),
+      { status: 422 }
+    );
+  }
+  return {
+    data: formData,
+  };
+};
